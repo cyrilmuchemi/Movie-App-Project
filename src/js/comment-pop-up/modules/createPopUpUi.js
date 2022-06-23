358e5fe5318d91d039214e1b816f228448f79721
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-23 15:26:18 +0530

    Make the call of comment and fetch data parallel using promise

--- src/js/comment-pop-up/modules/createPopUpUi.js
+++ src/js/comment-pop-up/modules/createPopUpUi.js
@@ -1,8 +1,5 @@
 import DataFactory from './moviesModel.js';
-import API from './api.js';
 import { CountComments } from './countData.js';
-
-const api = new API();
 
 const IMAGE = document.getElementById('show-img');
 const TITLE = document.getElementById('show-title');
@@ -10,23 +7,40 @@ const SHOW_DETAILS = document.getElementById('show-details');
 const COMMENT_LIST = document.getElementById('comment-list');
 const COMMENT_COUNT = document.getElementById('comment-count');
 
-export default (input) => {
-  const data = new DataFactory(input);
-  IMAGE.src = data.image;
-  IMAGE.alt = data.title;
-  TITLE.innerText = data.title;
-  SHOW_DETAILS.innerHTML = data.list.map(({ name, value }) => `<li class="show-detail-list-items">
-        <h3>${name}:</h3>
-        <h3>${value}</h3>
-    </li>`).join(' ');
-
-  api.getComments(data.id).then((comments) => {
+const commentListUI = (comments, data) => new Promise((resolve, reject) => {
+  if (CountComments(comments) > 0) {
     COMMENT_COUNT.innerText = `( ${CountComments(comments)} )`;
     COMMENT_LIST.innerHTML = comments.map((obj) => `<li class="comment-list-item">
         ${data.getDate(obj.creation_date)} ${obj.username}: ${obj.comment}
         </li>`).join('');
-  }).catch((err) => {
-    COMMENT_COUNT.innerText = '( 0 )';
-    COMMENT_LIST.innerHTML = `<li class="comment-list-item">${err.message}</li>`;
+    resolve(comments);
+  } else {
+    reject(new Error('No Comments'));
+  }
+});
+
+const errorCommentsGet = (err) => {
+  COMMENT_COUNT.innerText = '( 0 )';
+  COMMENT_LIST.innerHTML = `<li class="comment-list-item">${err.message}</li>`;
+};
+
+export { commentListUI, errorCommentsGet };
+
+export default (input) => {
+  const data = new DataFactory(input);
+
+  return new Promise((resolve, reject) => {
+    IMAGE.src = data.image;
+    IMAGE.alt = data.title;
+    TITLE.innerText = data.title;
+    SHOW_DETAILS.innerHTML = data.list.map(({ name, value }) => `<li class="show-detail-list-items">
+        <h3>${name}:</h3>
+        <h3>${value}</h3>
+    </li>`).join(' ');
+    if (IMAGE.src === data.image) {
+      resolve(data);
+    } else {
+      reject(new Error('Cannot Create Pop Up'));
+    }
   });
 };
